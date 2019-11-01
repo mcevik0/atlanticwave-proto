23204cc700c6d42210492ced68379507b9cb12c0
Sean Donovan <sdonovan@gatech.edu>
2019-11-01 13:40:13 -0400

    #109 - Trying to wrap the output as str instead of bytes?

--- sdxctlr/tests/test_RestAPI.py
+++ sdxctlr/tests/test_RestAPI.py
@@ -466,10 +466,10 @@ class EP_LOCALCONTROLLERLCINT_Test(EndpointTestCase):
         suffix = re.sub(r'(<[a-zA-Z]*>)', "FAKELC", EP_LOCALCONTROLLERLCINT)
         endpoint = ENDPOINT_PREFIX + suffix
 
-        output = subprocess.check_output(['curl', '-X', 'GET',
-                                          '-H', "Accept: application/json",
-                                          endpoint,
-                                          '-b', self.cookie_file])
+        output = str(subprocess.check_output(['curl', '-X', 'GET',
+                                              '-H', "Accept: application/json",
+                                              endpoint,
+                                              '-b', self.cookie_file]))
         expected_output = '{}'
         self.logger.error("output: %s" % output)
         if str(output) == "":
